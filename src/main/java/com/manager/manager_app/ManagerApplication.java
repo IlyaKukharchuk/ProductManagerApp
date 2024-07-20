1cd970287e87a55741eb43ef099917757e80bae3
Ilya Kukharchuk <iliakuharchuk@mail.ru>
2024-07-20 16:13:31 +0300

    corrected class names

--- src/main/java/com/manager/manager_app/ManagerApplication.java
+++ src/main/java/com/manager/manager_app/ManagerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ManagerAppApplication {
+public class ManagerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ManagerAppApplication.class, args);
+		SpringApplication.run(ManagerApplication.class, args);
 	}
 
 }
